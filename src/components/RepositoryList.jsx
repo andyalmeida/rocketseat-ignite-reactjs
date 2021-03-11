21b129e5e8f84b94ba13655c374767ff54e4a7d5
André Almeida <andalvalmeida@gmail.com>
2021-03-10 22:16:20 -0300

    Creating components

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+export default function RepositoryList() {
+  return (
+    <section className="repositoryList">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
